1d4ac8a16262d044a776dfe9f248967f40b03dde
Drysdale Chivhanga <chivhangadrys@gmail.com>
2020-10-13 03:17:38 +0200

    Console Tester Ui added

---
+++ drysdale-string-manipulation/enums-management.cs
@@ -0,0 +1,17 @@
+﻿namespace Drysdale.String.Manipulation
+{
+    /// <summary>
+    /// Enumeration.Charactor-Distinction
+    /// </summary>
+    public enum CharactorDistinction
+    {
+        /// <summary>
+        /// "Same as 'D' and 'd'" Capital-Letter And Small-Letter Are-Treated As-One Charactor
+        /// </summary>
+        CapitalAndSmallAreOne,
+        /// <summary>
+        /// "Same as 'D' and 'd'" Capital-Leter And Small-Letter Are-Treated As-Two-Different Charactors
+        /// </summary>
+        CapitalAndSmallAreTwo
+    }
+}
